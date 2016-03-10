1b9865d2682c8ea569794d6dc0372f5e980d4d62
skennedy6 <scott_kennedy@intuit.com>
2016-03-09 16:42:58 -0800

    Functional installer that also controls LaunchDaemons

--- post_install.sh
+++ post_install.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-cp authsightd.plist /Library/LaunchDaemon/org.nogas.authsightd.plist
-sudo chown root:wheel /Library/LaunchDaemons/org.nogas.authsightd.plist
-chmod 644 /Library/LaunchDaemons/org.nogas.authsightd.plist
-launchctl load -w /Library/LaunchDaemons/org.nogas.authsightd.plist
+/bin/launchctl load -w /Library/LaunchDaemons/org.nogas.authsightd.plist
 
