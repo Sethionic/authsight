2ee721de30b48d2590ecca40cefad7e76869cbf8
skennedy6 <scott_kennedy@intuit.com>
2016-04-27 17:10:29 -0700

    Replaced praudit usage with perl code to read binary feed from auditpipe

--- authsightd.pl
+++ authsightd.pl
@@ -1,20 +1,27 @@
 #!/usr/bin/env perl
 
 use strict;
-use IO::Handle;
-use MIME::Base64;
+use Fcntl;
 use File::stat;
+use IO::File;
+use IO::Handle;
+use IO::Select;
+use MIME::Base64;
+use POSIX qw(:errno_h);
 use vars qw { $IMAGESNAP $LOGDIR $LOGFILE $LAST $EMAIL $AIRPORT $IFCONFIG };
 
 $| = 1;
 
 $IMAGESNAP     = "/usr/local/bin/imagesnap";
 $LOGDIR        = "/var/log/AuthSight";
-$LOGFILE       = "/var/audit/current";
+$LOGFILE       = "/dev/auditpipe";
 $EMAIL         = "";
 $IFCONFIG      = "/sbin/ifconfig";
 $AIRPORT       = "/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport";
+my $HEADER_SIZE = 18;
+use constant READ_SIZE => 16*1024;
 
+#Start of code
 mkdir($LOGDIR) if (! -d $LOGDIR);
 &log("startup");
 &log("reading configuration");
@@ -24,59 +31,76 @@ if ($EMAIL eq "") {
 } else { 
     &log("logging and reporting to $EMAIL");
 }
-open(TAIL, "audit -n ; sleep 1 ; tail -F $LOGFILE | praudit -l|");
-for (;;) {
-    while(<TAIL>) {
-        chomp;
-        my($user) = "";
-        if ( (/user authentication,.*,text,Authentication for user <([A-Za-z0-9]*)>,return,failure/i) or 
-		(/user authentication,.*,text,Verify password for record type Users '([A-Za-z0-9]*)'.*,return,failure/i) or
-		(/SecSrvr authinternal mech,.*,text,user <([A-Za-z0-9]*)>,return,failure/i) or
-		(/SecSrvr authinternal mech,.*,text,Error opening DS node for user <([A-Za-z0-9]*)>,return,failure/i)
-	) {
-            if (defined($1)) {
-               $user = $1;
-               if ($LAST eq $_) {
-                   $LAST = $_;
-                   &log("strange dupe error. ignoring: $_");
+
+# Open the auditpipe, this should never close.
+sysopen(my ($fh), $LOGFILE, O_RDONLY|O_NONBLOCK) || &log("Couldn't open $LOGFILE for reading: $!\n");
+
+binmode($fh) || &log("can't binmode $LOGFILE") ;
+
+my $sel = new IO::Select ($fh); 
+
+for (;;) { # Loop indefinitely, incase auditpipe is closed 
+   my $buf = '';
+   my $remaining_bytes;
+   while ($sel->can_read()) {
+      my $rv = sysread($fh, $buf, READ_SIZE, length($buf)); &log("Failed to fill buffer $!\n") if !defined($rv); last if !$rv;
+      while ($buf =~ s/^(.{$HEADER_SIZE})//s) {
+         my($user) = "";
+         my $msg=$1;
+         my ($header_token_ID, $header_byte_count, $header_version, $header_event_type, $header_event_modifier, $header_epoch_seconds, $header_milliseconds ) = unpack 'H2 H8 H2 H4 H4 H8 H8', $msg;
+         $remaining_bytes = hex($header_byte_count) - $HEADER_SIZE ;
+         if (length($buf) < $remaining_bytes) { my $rv = sysread($fh, $buf, READ_SIZE, length($buf)); &log("Failed to fill buffer $!\n") if !defined($rv); last if !$rv; }
+         $buf =~ s/^(.{$remaining_bytes})//s;
+         my ($remainder_of_record)=$1;
+         if (hex($header_event_type) eq "45023") {
+            if ( ($remainder_of_record =~ /.*Authentication for user <([A-Za-z0-9]*)\x0\x27[^\x0]>/i) or 
+                 ($remainder_of_record =~ /.*Verify password for record type Users '([A-Za-z0-9]*)'.*\x0\x27[^\x0]/i) or
+                 ($remainder_of_record =~ /.*user <([A-Za-z0-9]*)>\x0\x27[^\x0]/i) or
+                 ($remainder_of_record =~ /.*Error opening DS node for user <([A-Za-z0-9]*)>\x0\x27[^\x0]/i)
+            ) {
+               if (defined($1)) {
+                  $user = $1;
+                  if ($LAST eq $_) {
+                      $LAST = $_;
+                      &log("strange dupe error. ignoring: $_");
+                  }
+                  $LAST = $_;
+	       } else {
+                  $user="unknown";
                }
-               $LAST = $_;
-	    } else {
-               $user="unknown";
-            }
-            my($sec, $min, $hour, $day, $mon, $year) = (localtime(time));
-            $year += 1900;
-            $mon  ++;
-            my($date) = sprintf("%02d-%02d-%04d_%02d.%02d.%02d", $mon, $day, $year, $hour, $min, $sec);
-            my($file) = "$LOGDIR/$user\_$date.jpg";
-            my($result) = `osascript -e 'do shell script "$IMAGESNAP $file"' >> /var/log/authsight.log`;
-            chomp($result);
-            &log("CAPTURE ON $_");
-            &log("$file $result");
+               my($sec, $min, $hour, $day, $mon, $year) = (localtime(time));
+               $year += 1900;
+               $mon  ++;
+               my($date) = sprintf("%02d-%02d-%04d_%02d.%02d.%02d", $mon, $day, $year, $hour, $min, $sec);
+               my($file) = "$LOGDIR/$user\_$date.jpg";
+               my($result) = `osascript -e 'do shell script "$IMAGESNAP $file"' >> /var/log/authsight.log`;
+               chomp($result);
+               &log("CAPTURE ON $_");
+               &log("$file $result");
 
-            if ($EMAIL ne "") {
-                my($data);
-                my($stat) = stat($file);
-                my($size) = $stat->size();
-                &log("emailing photo to $EMAIL size=" . $size);
-                open(FILE, "<$file");
-                binmode(FILE);
-                read(FILE, $data, $size, 0);
-                close(FILE);
-                &email($data);
-            }
-	}
-    }
-
-    select(undef, undef, undef, .20);
-
-    if (stat(*TAIL)->nlink == 0) {
-        &log("re-opening $LOGFILE on new filehandle");
-        close(TAIL);
-        open(TAIL, "audit -n ; sleep 1 ; tail -F $LOGFILE | praudit -l|") || &log("failed to re-open file: $!");
+               if ($EMAIL ne "") {
+                   my($data);
+                   my($stat) = stat($file);
+                   my($size) = $stat->size();
+                   &log("emailing photo to $EMAIL size=" . $size);
+                   open(FILE, "<$file");
+                   binmode(FILE);
+                   read(FILE, $data, $size, 0);
+                   close(FILE);
+                   &email($data);
+               }
+             }
+         }
+         if (length($buf) < $HEADER_SIZE) { my $rv = sysread($fh, $buf, READ_SIZE, length($buf)); &log("Failed to fill buffer $!\n") if !defined($rv); last if !$rv; }
+      }
+   }
+    if (stat($fh)->nlink == 0) {
+       &log("re-opening $LOGFILE on new filehandle");
+       close($fh);
+       sysopen(my ($fh), $LOGFILE, O_RDONLY|O_NONBLOCK) || &log("Couldn't open $LOGFILE for reading: $!\n");
+       binmode($fh) || &log("can't binmode $LOGFILE");
         &log("file re-oened");
-    }
-    seek(TAIL, 0, 1);
+   }
 }
 
 sub log {
